eb644e6991c6b403e1ba5b76175b53226a668e55
Mohammed Abdul Aziz <cloudaziz@gmail.com>
2023-07-23 19:59:11 +0600

    adding javaSscript

---
+++ script.js
@@ -0,0 +1,40 @@
+let sliderWrapper = document.querySelector('.slider-wrapper');
+let sliderWrapperWidth = sliderWrapper.clientWidth;
+let slider = document.querySelector('.slider ul');
+
+let sliderItem = slider.querySelectorAll('li');
+let totalSliderItem = sliderItem.length;
+
+let sliderItemWidth = sliderItem[0].clientHeight;
+
+let root = document.querySelector(':root');
+let showItem = getComputedStyle(root).getPropertyValue('--show-items');
+let itemGap = getComputedStyle(root).getPropertyValue('--item-gap');
+
+let showItemWidth = sliderItemWidth * showItem;
+
+let totalItemWidth = totalSliderItem * sliderItemWidth;
+
+let count = 0;
+
+document.querySelector('.next').addEventListener('click', () => {
+  if (count == totalSliderItem - showItem) return;
+
+  count = count + 1;
+
+  let move =
+    sliderWrapperWidth - sliderItemWidth - itemGap - sliderWrapperWidth;
+
+  slider.style.setProperty('--move', `${move * count}px`);
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+  if (count == 0) return;
+
+  count = count - 1;
+
+  let move =
+    sliderWrapperWidth - sliderItemWidth - itemGap - sliderWrapperWidth;
+
+  slider.style.setProperty('--move', `${move * count}px`);
+});
